6d2c0cb2befdfb9e7941c3f32e1838e3d031eb6a
Saif <mohamedsaifeddine.kamoun@insat.ucar.tn>
2024-05-15 18:16:42 +0100

    Item reactive , not sure if it works correctly

--- src/main/java/in/obify/obitemservice/repository/ItemRepository.java
+++ src/main/java/in/obify/obitemservice/repository/ItemRepository.java
@@ -2,10 +2,21 @@ package in.obify.obitemservice.repository;
 
 import in.obify.obitemservice.model.ItemModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-public interface ItemRepository extends MongoRepository<ItemModel, String> {
+@Repository
+public interface ItemRepository extends ReactiveCrudRepository<ItemModel, String> {
     public void deleteAllByCategoryId(String categoryId);
     //public List<ItemModel> findAllByCategoryId(String categoryId);
+    public Flux<ItemModel> findAll();
+    public Mono<ItemModel> findById(String id);
+    public Mono<ItemModel> insert(ItemModel model);
+
+    public Mono<ItemModel> save(ItemModel model);
+    public Mono<Void> deleteById(String id);
 }
